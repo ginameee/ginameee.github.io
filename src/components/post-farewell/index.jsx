fae2352b2f3dfbace406c8c11f441396904516ba
ginameee <ginameeee@gmail.com>
2021-03-19 20:22:53 +0900

    modify theme style and post-farewell text

--- src/components/post-farewell/index.jsx
+++ src/components/post-farewell/index.jsx
@@ -5,8 +5,8 @@ export const PostFarewell = () => {
   return (
     <p className="post-farewell">
       <small>
-        이 블로그는 제가 학습하며 알게된 내용을 정리해보는 공간으로, 주관적인
-        내용이 많이 담겨있습니다! <br />
+        이 블로그는 제가 알게된 내용을 정리해보는 공간으로, 주관적인 내용이 많이
+        담겨있습니다! <br />
         잘못된 내용이나 궁금한 점은 언제든지 댓글로 남겨주세요 :)
       </small>
     </p>
